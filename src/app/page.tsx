1a823a0d4c36fd038a4371d1af2dd5fee4e56489
Alex Flores Z <lexzum10@gmail.com>
2024-04-12 22:13:03 -0500

    Update redirect path in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import { redirect } from 'next/navigation'
 
 export default function Home() {
-  return redirect('/home')
+  return redirect('/login')
   /* return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       hola mian
